e8ddf6acf69bfdf91b794912d03724883046c448
Anuragk002 <anuragkushwaha656@gmail.com>
2020-09-02 20:38:10 +0530

    cart page and auth

--- server.js
+++ server.js
@@ -8,14 +8,23 @@ const PORT=process.env.PORT || 3300
 //assets
 app.use(express.static('public'))
 
+// always keep this expressLayout part above routes
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
-
-app.use(expressLayout)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening on port....... ${PORT}` )
